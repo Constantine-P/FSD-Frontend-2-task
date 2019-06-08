aee498aeade387532e2455a6efad3b2a27ecf4d9
Constantine P <pkv989@gmail.com>
2019-06-08 15:04:45 +0600

    В StayPeriodControl добавил автопереключение на выбор даты выезда после выбора даты въезда

--- source/scripts/StayPeriodControl.js
+++ source/scripts/StayPeriodControl.js
@@ -220,6 +220,7 @@ export default function StayPeriodControl(id) {
 		addMarkedClass(e);
 		fillPeriodOfStay();
 		swapArrDepDays();
+		if (focusedDay === 'arrival-day') inputDeparture.focus();
 	}
 
 	function detectFocusedDay() {
