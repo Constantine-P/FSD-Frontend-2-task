8b71ec4ce58e2f5ecfa54cf206d91540770d54be
Constantine P <pkv989@gmail.com>
2019-07-02 00:09:21 +0600

    Сверстал страницу room-details, включая вложенные блоки

---
+++ source/pages/room-details/room-details.js
@@ -0,0 +1,7 @@
+import './room-details.styl';
+import PosControl from '../../scripts/PosControl';
+import StayPeriodControl from '../../scripts/StayPeriodControl';
+
+new PosControl('guests-control');
+new StayPeriodControl('stay-period-control');
+
